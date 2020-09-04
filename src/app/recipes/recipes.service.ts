d50e7203e112f0f6f55ebf68dd3f802771acff7c
Juan Carlos Peña Peña <jcpena@stefanini.com>
2020-09-04 06:24:21 -0500

    Managing Recipes in a Recipe Service

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,15 @@
+import { Recipe } from './recipe.model';
+
+export class RecipesService {
+
+  private recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simple a test',
+    'https://www.foxandbriar.com/wp-content/uploads/2019/04/Creamy-Shrimp-with-basil-and-roasted-Red-peppers-3-of-7.jpg'),
+    new Recipe('Another test recipe', 'This is simple a test',
+    'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
