37589249781eda9a7182f12e78e743aa5a65f215
Juan Carlos Peña Peña <penapena@uninorte.edu.co>
2020-09-05 07:02:38 -0500

    Adding ingredients to recipe

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipesService {
 
@@ -7,9 +8,11 @@ export class RecipesService {
 
   private recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is simple a test',
-    'https://www.foxandbriar.com/wp-content/uploads/2019/04/Creamy-Shrimp-with-basil-and-roasted-Red-peppers-3-of-7.jpg'),
+    'https://www.foxandbriar.com/wp-content/uploads/2019/04/Creamy-Shrimp-with-basil-and-roasted-Red-peppers-3-of-7.jpg',
+    [new Ingredient('Bread', 3), new Ingredient('Meat', 7)]),
     new Recipe('Another test recipe', 'This is simple a test',
-    'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg')
+    'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg',
+    [new Ingredient('Tomatoes', 5), new Ingredient('Cheese', 4)])
   ];
 
   getRecipes() {
